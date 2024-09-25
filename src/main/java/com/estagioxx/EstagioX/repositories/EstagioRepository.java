ec65acb9a9fd09fbea0b6cf7ad15d374ad72d993
Gustavo Nascimento <santos025gn@gmail.com>
2024-09-24 22:14:29 -0300

    fix: EstagioRepository

--- src/main/java/com/estagioxx/EstagioX/repositories/EstagioRepository.java
+++ src/main/java/com/estagioxx/EstagioX/repositories/EstagioRepository.java
@@ -1,8 +1,12 @@
 package com.estagioxx.EstagioX.repositories;
 
 import com.estagioxx.EstagioX.entities.Estagio;
+import com.estagioxx.EstagioX.entities.OfertaEstagio;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
+
 public interface EstagioRepository extends JpaRepository<Estagio, Long> {
-    boolean existsByOfertaEstagio_IdOfertaEstagio(Long idOfertaEstagio);
+
 }
